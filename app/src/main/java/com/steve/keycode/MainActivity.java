b8874f89e729428d1c04aaa5de0d60684a66ce51
cis-130 <steveit1999@gmail.com>
2019-09-13 12:10:09 +0530

    first commit

---
+++ app/src/main/java/com/steve/keycode/MainActivity.java
@@ -0,0 +1,14 @@
+package com.steve.keycode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
